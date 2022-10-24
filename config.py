e1748859881732b3574c37de89b9fc68c1900c7b
MrNihalX <109473632+MrNihalX@users.noreply.github.com>
2022-10-24 23:08:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,13 +16,13 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 LOGGER_ID = int(getenv("LOGGER_ID"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "1697845783").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "926151330").split()))
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/8021f124a9a418cb61a98.jpg")
-START_IMG = getenv("START_IMG", "https://te.legra.ph/file/eec6dcc33de8add5ad2f0.jpg")
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/1aced97b4bf66aa2140a9.jpg")
 
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/The_Royal_Squad21")
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", " https://t.me/uknowrohit")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/+qX4DpUpNzv05NTY9")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+qX4DpUpNzv05NTY9")
 
 STRING_SESSION = getenv("STRING_SESSION", None)
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1697845783").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "926151330").split()))
